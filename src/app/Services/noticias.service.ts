617c887d6c203572bcb561ca79ece323c9c0fcf0
Alejandro García <45419684+mediadocena@users.noreply.github.com>
2019-11-28 09:56:20 +0100

    comment

--- src/app/Services/noticias.service.ts
+++ src/app/Services/noticias.service.ts
@@ -9,16 +9,6 @@ export class NoticiasService {
   constructor(private http:HttpClient) {
 
    }
-   /*this.http.get('http://localhost:3000/api/users').subscribe(
-    (response)=>{
-    const aux:[any]=[{}];
-    aux.push(response);
-    console.log(aux);
-    aux.forEach(element => {
-      this.datos=(element);
-    });
-  },(error) => console.log('error'));
-  console.log(this.datos);*/
    getNoticias(){
      return this.http.get('http://localhost:3000/api/noticias')
       }
