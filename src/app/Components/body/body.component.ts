310c73ae2c0579da214da4d2a2eea5d2b312f095
Alejandro García <45419684+mediadocena@users.noreply.github.com>
2019-12-05 20:38:50 +0100

    Prototipo obtener comentarios

--- src/app/Components/body/body.component.ts
+++ src/app/Components/body/body.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NoticiasService } from 'src/app/Services/noticias.service';
+import { Noticia } from 'src/app/Models/NoticiasModel';
+import { Comentarios } from 'src/app/Models/ComentariosModel';
 
 @Component({
   selector: 'app-body',
@@ -9,10 +11,14 @@ import { NoticiasService } from 'src/app/Services/noticias.service';
 export class BodyComponent implements OnInit {
  
   constructor(private noticiasService:NoticiasService) { }
-  noticias:any=[];
+  noticias:Noticia[] = [];
+  comentario:Comentarios[];
   ngOnInit() {
-    this.noticiasService.getNoticias().subscribe(  (data) => { 
-      this.noticias = data;
+    this.noticiasService.getNoticias().subscribe((data) => { 
+      this.noticias=data;
+      this.comentario = this.noticias[1].comentarios
+      console.log(this.noticias)
+      console.log(this.comentario)
     },
     (error) => {
       console.error(error);
---
+++ src/app/Models/ComentariosModel.ts
@@ -0,0 +1,10 @@
+export class Comentarios{
+    nick:string;
+    numero:number;
+    icono:string;
+    cuerpo:string;
+    constructor(){
+        
+    }
+
+}
