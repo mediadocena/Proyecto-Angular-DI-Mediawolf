0e2c57051faf3ca396994352f53c4fcc3921e6bb
Joaquin Villegas <joa.villan@outlook.com>
2020-01-01 22:48:47 +0100

    F footer

--- src/app/Components/crearnoticia/crearnoticia.component.ts
+++ src/app/Components/crearnoticia/crearnoticia.component.ts
@@ -16,6 +16,27 @@ export class CrearnoticiaComponent implements OnInit {
   titulo="";
   subtitulo="";
   categoria="Videojuegos";
+  config = {
+    "editable": true,
+    "spellcheck": true,
+    "height": "auto",
+    "minHeight": "950",
+    "width": "auto",
+    "minWidth": "0",
+    "translate": "yes",
+    "enableToolbar": true,
+    "showToolbar": true,
+    "placeholder": "Enter text here...",
+    "imageEndPoint": "",
+    "toolbar": [
+        ["bold", "italic", "underline", "strikeThrough", "superscript", "subscript"],
+        ["fontName", "fontSize", "color"],
+        ["justifyLeft", "justifyCenter", "justifyRight", "justifyFull", "indent", "outdent"],
+        ["cut", "copy", "delete", "removeFormat", "undo", "redo"],
+        ["paragraph", "blockquote", "removeBlockquote", "horizontalLine", "orderedList", "unorderedList"],
+        ["link", "unlink", "image", "video"]
+    ]
+}
   ngOnInit() {
   }
   publicar(){
