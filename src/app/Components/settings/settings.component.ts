9f10bb588de569fbd9643cdd875d39dc4455f03b
Alejandro García <45419684+mediadocena@users.noreply.github.com>
2020-02-03 13:45:28 +0100

    Settings Empezado

--- src/app/Components/settings/settings.component.ts
+++ src/app/Components/settings/settings.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ImagenesService } from 'src/app/Services/imagenes.service';
+import { UserServiceService } from 'src/app/Services/user-service.service';
 
 @Component({
   selector: 'app-settings',
@@ -6,10 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./settings.component.css']
 })
 export class SettingsComponent implements OnInit {
-
-  constructor() { }
+  file: any;
+  ext: any;
+  img: string;
+  nombreIcono:string;
+  userid;
+  constructor(private imageService:ImagenesService,private user:UserServiceService) { }
   opcion = 'cuenta';
   ngOnInit() {
+    this.user.obtenerUsuario().subscribe((resp:any)=>{
+      this.userid = resp.id;
+    });
   }
   cambiarOpcion(option){
     switch(option){
@@ -28,4 +37,33 @@ export class SettingsComponent implements OnInit {
     }
 
   }
+  handleFileSelect(evt){
+    var files = evt.target.files;
+    this.file = files[0];
+    this.ext=this.file.name;
+    this.ext = this.ext.slice((this.ext.lastIndexOf(".") - 1 >>> 0) + 2);
+  if (files && this.file) {
+      var reader = new FileReader();
+
+      reader.onload =this._handleReaderLoaded.bind(this);
+
+      reader.readAsBinaryString(this.file);
+  }
+}
+
+_handleReaderLoaded(readerEvt) {
+   var binaryString = readerEvt.target.result;
+          this.img= btoa(binaryString);
+          console.log(btoa(binaryString));
+  }
+  subirImagen(){
+    this.nombreIcono = this.nombreIcono+this.ext;
+    this.imageService.uploadImage(this.img, this.nombreIcono).subscribe(
+      (res) => {
+        alert('Se ha actualizado el icono correctamente');
+      },
+      (err) => {
+        alert('Ha ocurrido un error en la subida de la imagen:'+err.err);
+      })
+}
 }
